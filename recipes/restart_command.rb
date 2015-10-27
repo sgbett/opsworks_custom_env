18a879c6e1720aa444ecfb0b6ff620ce8f8b3fee
Simon Bettison <simon@bettison.org>
2015-10-27 21:43:04 +0000

    remove conditional

--- recipes/restart_command.rb
+++ recipes/restart_command.rb
@@ -2,11 +2,7 @@ node[:deploy].each do |application, deploy|
 
   execute "restart rails app #{application} for custom env" do
     cwd deploy[:current_path]
-    if node[:opsworks][:rails_stack][:name].eql? "apache_passenger"
-      command "touch #{deploy[:deploy_to]}/current/tmp/restart.txt"
-    elsif node[:opsworks][:rails_stack][:recipe].eql? "nginx_unicorn"
-      command "#{deploy[:deploy_to]}/shared/scripts/unicorn clean-restart"
-    end
+    command "#{deploy[:deploy_to]}/shared/scripts/unicorn clean-restart"
     user deploy[:user]
 
     action :nothing
